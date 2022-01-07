381665e9f1c26a347e8f6a9e6275ae2379915f25
Jonas Schmidt <schmidt.a.jonas@gmail.com>
2022-01-07 19:19:38 +0100

    added logger

---
+++ elysium/common/logger.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+from typing import Dict, Optional, Tuple , Union
+from torch.utils.tensorboard import SummaryWriter, writer
+
+from abc import ABC, abstractmethod
+
+# This logger is similar to the logger used in stable-baselines3
+
+
+class BaseLogger(ABC):
+    """
+    The logger class
+    
+    """
+    
+    def __init__(self) -> None:
+         pass
+     
+    
+    @abstractmethod
+    def log(self, key: str, value: Union[int, float], step: int):
+        pass
+    
+    
+     
+class TensorboardLogger(BaseLogger):
+    
+    def __init__(self, folder: str, print_to_console: bool = True) -> None:
+        super().__init__()
+        self.writer = SummaryWriter()
+        self.print_to_console = print_to_console
+        
+
+    def log(self, key: str, value: Union[int, float], step: int):
+        writer.add_scalar(key, value, step)
+        
+        if self.print_to_console:
+            print(f"{step}  -  {key}: {value} ")
+            
