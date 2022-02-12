950421b747a3bf8790d5a2ac968464b326e5ebe3
Jonas Schmidt <schmidt.a.jonas@gmail.com>
2022-02-12 22:02:12 +0100

    to the macbook

---
+++ tonian/algorithms/algo_parser.py
@@ -0,0 +1,23 @@
+
+from typing import Dict, Union
+
+
+class AlogParser:
+    
+    def __init__(self, config: Union[str, Dict]) -> None:
+        """Parses the config and creates an algorithm based on the config file
+        Args:
+            config (Union[str, Dict]): [description]
+        """
+        
+        if isinstance(config, str):
+            
+        elif isinstance(config, Dict):
+            
+        else:
+            throw E
+        
+        
+        
+        
+        pass
