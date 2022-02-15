6797111ab277a34f82f07909766e429dd700be7c
Jonas Schmidt <schmidt.a.jonas@gmail.com>
2022-02-15 16:12:03 +0100

    join configs

---
+++ tonian/tasks/base/task_parser.py
@@ -0,0 +1,11 @@
+
+import yaml
+
+import tonian.tasks.cartpole.cartpole_task
+import tonian.tasks.walking.walking_task
+
+class TaskParser():
+    
+    def __init__(self) -> None:
+        pass
+    
