53f545beccfe7e4cff7936c9fd93b11dad46639c
Jonas Schmidt <schmidt.a.jonas@gmail.com>
2022-03-22 00:47:43 +0100

    started with functional compmatmentalisation of the agent functions

--- tonian/tasks/mk1_walking/mk1_walking_task.py
+++ tonian/tasks/mk1_walking/mk1_walking_task.py
@@ -2,7 +2,7 @@ from gym.spaces import space
 import numpy as np
 from tonian.tasks.base.command import Command
 from tonian.tasks.base.vec_task import VecTask
-
+from tonian.tasks.agents.mk1_helper import *
 
 from isaacgym.torch_utils import torch_rand_float, tensor_clamp
 
@@ -16,14 +16,8 @@ from typing import Dict, Any, Tuple, Union, Optional
 from isaacgym import gymtorch, gymapi
 from isaacgym.torch_utils import to_torch
 
-import yaml
-import time
-import os
+import yaml, os, torch
 
-
-import torch
-from torch import nn 
-import torch.nn as nn
 
 class Mk1WalkingTask(VecTask):
     
@@ -100,7 +94,7 @@ class Mk1WalkingTask(VecTask):
         self.dof_force_tensor = gymtorch.wrap_tensor(dof_force_tensor).view(self.num_envs, self.num_dof)
         
         # the amount of sensors each env has
-        sensors_per_env = len(self.parts_with_force_sensor)
+        sensors_per_env = len(parts_with_force_sensor)
         self.vec_force_sensor_tensor = gymtorch.wrap_tensor(sensor_tensor).view(self.num_envs, sensors_per_env, 6)
         
         self.refresh_tensors()
@@ -133,39 +127,20 @@ class Mk1WalkingTask(VecTask):
         upper = gymapi.Vec3(0.5 * spacing, spacing, spacing)
 
 
-        asset_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../assets/urdf/mk-1/")
-        
-        mk1_robot_file = "robot.urdf"
-        
-        asset_options = gymapi.AssetOptions()
-        asset_options.default_dof_drive_mode = gymapi.DOF_MODE_EFFORT
-        asset_options.fix_base_link = False
-        asset_options.disable_gravity = False
-        # todo randomize and add other asset_options
-        
-        mk1_robot_asset = self.gym.load_asset(self.sim, asset_root, mk1_robot_file, asset_options)
-        
-        self.num_dof = self.gym.get_asset_dof_count(mk1_robot_asset)
-        
-        # --- add the force sensors for the arms torso, feet, head, knees
-        
-        sensor_pose = gymapi.Transform()
-        
-        self.parts_with_force_sensor = ['foot', 'foot_2', 'forearm', 'forearm_2']
-        parts_idx = [self.gym.find_asset_rigid_body_index(mk1_robot_asset, part_name) for part_name in self.parts_with_force_sensor]
+        mk1_robot_asset = create_mk1_asset(self.gym, self.sim)
 
-        for part_idx in parts_idx:
-            self.gym.create_asset_force_sensor(mk1_robot_asset, part_idx, sensor_pose)
         
-        
-        
-        start_pose = gymapi.Transform()
+        self.num_dof = self.gym.get_asset_dof_count(mk1_robot_asset) 
+                
+                
         self.robot_handles = []
         self.envs = [] 
         
         
+        start_pose = gymapi.Transform()
         start_pose.p = gymapi.Vec3(0.0,0.0, 1.80)
         start_pose.r = gymapi.Quat(0.0, 0.0, 0.0, 1.0)
+        
         for i in range(self.num_envs):
             # create env instance
             env_ptr = self.gym.create_env(
@@ -315,6 +290,9 @@ class Mk1WalkingTask(VecTask):
         pass
     
 
+    
+    
+
 @torch.jit.script
 def compute_robot_rewards(root_states: torch.Tensor,
                           former_root_states: torch.Tensor,
@@ -366,66 +344,3 @@ def compute_robot_rewards(root_states: torch.Tensor,
          
 
         return (reward, has_fallen)
-
-@torch.jit.script
-def compute_linear_robot_observations(root_states: torch.Tensor, 
-                                sensor_states: torch.Tensor, 
-                                dof_vel: torch.Tensor, 
-                                dof_pos: torch.Tensor, 
-                                dof_limits_lower: torch.Tensor,
-                                dof_limits_upper: torch.Tensor,
-                                dof_force: torch.Tensor,
-                                actions: torch.Tensor,
-                                initial_heading: torch.Tensor
-                                ):
-    
-    
-    """Calculate the observation tensors for the crititc and the actor for the humanoid robot
-    
-    Note: The resulting tensors must be in the same shape as the multispaces: 
-        - self.actor_observation_spaces
-        - self.critic_observatiom_spaces
-
-    Args:
-        root_states (torch.Tensor): Root states contain things like positions, velcocities, angular velocities and orientation of the root of the robot 
-        sensor_states (torch.Tensor): state of the sensors given 
-        dof_vel (torch.Tensor): velocity tensor of the dofs
-        dof_pos (torch.Tensor): position tensor of the dofs
-        dof_force (torch.Tensor): force tensor of the dofs
-        actions (torch.Tensor): actions of the previous 
-
-    Returns:
-        Tuple[Dict[torch.Tensor]]: (actor observation tensor, critic observation tensor)
-    """
-    
-    
-    torso_position = root_states[:, 0:3]
-    torso_rotation = root_states[:, 3:7]
-    velocity = root_states[:, 7:10]
-    ang_velocity = root_states[:, 10:13]
-    
-    # print("Torso Position")
-    # print(torso_position.shape)
-    # print(torso_rotation.shape)
-    # print(velocity.shape)
-    # print(ang_velocity.shape)
-    # print(sensor_states.shape)
-    
-    
-    
-    # todo add some other code to deal with initial information, that might be required
-    
-    
-    # todo: the actor still needs a couple of accelerometers
-    linear_actor_obs = torch.cat((sensor_states.view(root_states.shape[0], -1), dof_pos, dof_vel, dof_force, ang_velocity, torso_rotation, actions), dim=-1)
-    
-    
-    # print('actor_obs')
-    # print(linear_actor_obs.shape)
-    linear_critic_obs = torch.cat((linear_actor_obs, torso_rotation, velocity, torso_position, actions), dim=-1)
-    
-    # print('critic_obs')
-    # print(linear_critic_obs.shape)
-    return  linear_actor_obs,   linear_critic_obs
-    
-    
