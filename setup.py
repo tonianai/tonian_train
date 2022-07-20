b4c0b8efdb1e7451708bee3bbcb9bc80b1151149
Jonas Schmidt <schmidt.a.jonas@gmail.com>
2022-07-20 21:13:15 +0200

    added setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name='tonian_train',
+    packages=find_packages(),
+    version='0.0.1',
+    description='The training library used to train the simulated tonian robots',
+    author='Jonas Schmidt',
+    install_requires=['torch', 'numpy', 'gym', 'tensorboard', 'pyyaml'],
+    
+)
